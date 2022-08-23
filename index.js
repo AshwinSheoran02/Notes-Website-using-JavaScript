8bdfda314bb88926ebc34da59cb02d4d8c526630
Ashwin Sheoran <88393756+AshwinSheoran02@users.noreply.github.com>
2022-08-23 17:51:33 +0530

    Done

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 console.log("Hi this is the JS file for Magic Notes app");
 
-showNotes();
+viewNotes();
 
-let addbtn = document.getElementById("Submit")
-addbtn.addEventListener("click" , function(e){
-    let txt = document.getElementById("txt");
-    let notes = localStorage.getItem("notes");
-    if (notes==null){
-        Noteslst =[]
-    }
-    else{
-        Noteslst  = JSON.parse(notes)
-    }
-    notes.push(txt.value);
-    localStorage.setItem("notes" , JSON.stringify("notes"));
-    txt.value="";
-    showNotes();
+let addBtn = document.getElementById("addBtn");
+console.log("clicked")
+addBtn.addEventListener("click", function(e) {
+  let notetxt = document.getElementById("notetxt");
+  let notes = localStorage.getItem("notes");
+    console.log(notes)
+
+  if (notes == null) {
+    Noteslst = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  //console.log(notetxt)
+  Noteslst.push(notetxt.value);
+  localStorage.setItem("notes", JSON.stringify(Noteslst));
+  txt.value = "";
+
+  viewNotes();
 
 });
 
 
-function showNotes(){
-    let allNotes = localStorage.getItem("allNotes");
-    if (allNotes==null){
-        Noteslst =[];
-    }
-    else{
-        Noteslst  = JSON.parse(allNotes)
+function viewNotes(){
+    let notes = localStorage.getItem("notes");
+    console.log(notes);
+    if (notes == null) {
+      Noteslst = [];
+    } else {
+      Noteslst = JSON.parse(notes);
     }
     let htmlquery="";
     Noteslst.forEach(function(element, index) {
-        //console.log("html+");
-        html+= `
+        console.log("html+");
+        htmlquery+= `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
@@ -43,13 +47,26 @@ function showNotes(){
         `
 
     });
+    console.log(Noteslst);
 
     let notesElm = document.getElementById("notes");
     if (Noteslst.length != 0) {
-        notesElm.innerHTML = html;
+        notesElm.innerHTML = htmlquery;
       } else {
         notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
       }
+}
+
+function deleteNote(index){
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        Noteslst = [];
+      } else {
+        Noteslst = JSON.parse(notes);
+      }
+      Noteslst.splice(index, 1);
+      localStorage.setItem("notes", JSON.stringify(Noteslst));
+      viewNotes();
 }
 
 
