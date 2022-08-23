ed0247d35df9e782b1951a2f84505b3cedfbb1e5
Ashwin Sheoran <88393756+AshwinSheoran02@users.noreply.github.com>
2022-08-23 17:07:17 +0530

    started js

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ console.log("Hi this is the JS file for Magic Notes app");
 showNotes();
 
 let addbtn = document.getElementById("Submit")
+addbtn.addEventListener("click" , function(e){
+    let txt = document.getElementById("txt");
+    let allNotes = localStorage.getItem("allNotes");
+    if (allNotes==null){
+        Noteslst =[]
+    }
+    else{
+        Noteslst  = JSON.parse(allNotes)
+    }
+    allNotes.push(txt.value);
+    localStorage.setItem("allnotes" , JSON.stringify("allnotes"));
+    txt.value="";
+    showNotes();
+
+});
