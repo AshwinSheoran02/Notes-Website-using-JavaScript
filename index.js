6286ee195b96c79cd6f1040f495cca69fc632ee3
Ashwin Sheoran <88393756+AshwinSheoran02@users.noreply.github.com>
2022-08-23 17:17:29 +0530

    js mid

--- index.js
+++ index.js
@@ -18,3 +18,34 @@ addbtn.addEventListener("click" , function(e){
     showNotes();
 
 });
+
+function showNotes(){
+    let allNotes = localStorage.getItem();
+    if (allNotes==null){
+        Noteslst =[]
+    }
+    else{
+        Noteslst  = JSON.parse(allNotes)
+    }
+    let htmlquery="";
+    Noteslst.forEach(function(element, index) {
+        html+= `
+        <div class="card" style="padding:5px">
+        <h5 class="card-title">Note ${index + 1}</h5>
+        <p class="card-text"> ${element}</p>
+        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+        </div>
+        
+        `
+
+    });
+    
+    let notesElm = document.getElementById("notes");
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+      } else {
+        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+      }
+}
+
+
